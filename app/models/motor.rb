e8ad80d29ab97f25c85729aa082d18f840339803
Dan Smith <dan@getabetterpic.com>
2020-02-01 14:56:06 -0500

    Load motor info from RSE file

--- app/models/motor.rb
+++ app/models/motor.rb
@@ -1,3 +1,23 @@
 class Motor < ApplicationRecord
   belongs_to :manufacturer
+
+  def self.from_rse(file)
+    f = File.open(file, 'rb')
+    parsed = Nokogiri::XML(f)
+    parsed.css('engine-database engine-list engine').each do |engine|
+      manufacturer = Manufacturer.find_or_create_by!(name: engine.attributes['mfg'].value.strip)
+      code = engine.attributes['code'].value
+      new_motor = find_or_initialize_by(code: code, manufacturer: manufacturer)
+      new_motor.single_use = engine.attributes['Type'].value != 'reloadable'
+      new_motor.reloadable = engine.attributes['Type'].value == 'reloadable'
+      new_motor.diameter = engine.attributes['dia'].value.to_i
+      new_motor.specs = engine.attributes.collect { |k, v| [k, v.value] }.to_h
+      new_motor.thrust_curve = engine.css('data')[0].css('eng-data').collect do |data|
+        data.attributes.collect { |k, v| [k, v.value] }.to_h
+      end
+      new_motor.save!
+    end
+  ensure
+    f.close
+  end
 end
